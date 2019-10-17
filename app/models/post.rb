6c011b7aefcdd2f4984bb7760636298c62a59e3a
Yair fernando <yair.facio11@gmail.com>
2019-10-17 18:55:02 -0500

    Adding the comments controller and the functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 25}
+  validates :content, presence: true, length: { minimum: 10, maximum: 200}
+  has_many :comments
+
 end
