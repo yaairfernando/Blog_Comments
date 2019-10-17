347ac93b5ff3d02f0ef623475533b53212459d9f
Yair fernando <yair.facio11@gmail.com>
2019-10-17 16:56:03 -0500

    Finish implementation of Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,58 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:info] = "You have successfully created a new post"
+      #This is exactly the same
+      redirect_to @post
+      # redirect_to post_path(@post)
+      #Except this, redirects to the root path
+      # redirect_to root_path
+    else
+      render 'new'
+    end
 
+  end
+
+  def show
+   
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+      flash[:info] = "You have been successfully updated this post"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    flash[:danger] = "You have successfully deleted this post"
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
